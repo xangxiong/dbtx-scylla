9453600373ff7ed77628d184b0bc81f7eafd3462
Xang Xiong <xang.xiong@commscope.com>
2024-03-22 00:52:24 +0000

    refactor

--- dbt/adapters/scylla/__init__.py
+++ dbt/adapters/scylla/__init__.py
@@ -1,13 +1,13 @@
 # these are mostly just exports, #noqa them so flake8 will be happy
-from dbt.adapters.scylla.connections import CockroachdbConnectionManager  # noqa
-from dbt.adapters.scylla.connections import CockroachdbCredentials
-from dbt.adapters.scylla.column import CockroachdbColumn  # noqa
-from dbt.adapters.scylla.relation import CockroachdbRelation  # noqa: F401
-from dbt.adapters.scylla.impl import CockroachdbAdapter
+from dbt.adapters.scylla.connections import ScyllaConnectionManager  # noqa
+from dbt.adapters.scylla.connections import ScyllaCredentials
+from dbt.adapters.scylla.column import ScyllaColumn  # noqa
+from dbt.adapters.scylla.relation import ScyllaRelation  # noqa: F401
+from dbt.adapters.scylla.impl import ScyllaAdapter
 
 from dbt.adapters.base import AdapterPlugin
-from dbt.include import cockroachdb
+from dbt.include import scylla
 
 Plugin = AdapterPlugin(
-    adapter=CockroachdbAdapter, credentials=CockroachdbCredentials, include_path=cockroachdb.PACKAGE_PATH
+    adapter=ScyllaAdapter, credentials=ScyllaCredentials, include_path=scylla.PACKAGE_PATH
 )
